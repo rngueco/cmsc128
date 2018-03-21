ef57f3abb97c156873ff5da4d07f798963c87cdc
mtnolasco <31950502+mtnolasco@users.noreply.github.com>
2018-03-21 14:25:05 +0800

    Add files via upload
    medj crude pa to pero meron na yung symmetry(?) at combination tas powers of two

---
+++ draft 1/js/app.js
@@ -0,0 +1,104 @@
+var type = "WebGL";
+
+if(!PIXI.utils.isWebGLSupported()){
+  type = "canvas";
+}
+
+PIXI.utils.sayHello(type);
+
+var renderer = PIXI.autoDetectRenderer(1024, 1024, {
+	transparent: true,
+	resolution: 1,
+	antialias: true
+});
+
+document.getElementById('display').appendChild(renderer.view);
+
+var stage = new PIXI.Container();
+stage.interactive = true;
+
+var n = 10;
+
+var hexagons = [];
+var size = 60;
+var fWid = n*size;
+
+var offX = fWid/2+100;
+var offY = 100;
+
+var tempX = offX;
+for (var i = 0; i<n; i++) {
+	var c = 1;
+	var row = [];
+	
+	offX = tempX-(i-1)*size/2;
+
+	for (var j = 0; j<=i; j++) {
+		var newhex = new hexagon();
+		newhex.setup(stage);
+		newhex.size = size;
+
+		newhex.x = offX;
+		newhex.y = offY;
+
+		newhex.row = i;
+		newhex.column = j;
+		
+		newhex.value = c;
+		c = parseInt(c*((i+1)-(j+1))/(j+1));
+		
+		//for symmetry
+		
+		if(j <= parseInt(i/2)){
+			var k = j;
+		} else {
+			var k = i-j;
+		}
+		if(k%3 == 0){
+			newhex.bgColor = 0xccffee;
+		} else if(k%3 == 1){
+			newhex.bgColor = 0xddeeff;
+		} else{
+			newhex.bgColor = 0xffeedd;
+		}
+		
+		//setting index for pattern stuff
+		newhex.indx = j;
+		newhex.indy = i;
+		
+		row.push(newhex);
+
+		offX += size;
+	}
+
+	offY += size;
+
+	hexagons.push(row);
+}
+
+var lastTime = 0;
+
+PIXI.loader.load(setup);
+
+function setup() {
+
+	// update
+	animate(0);
+}
+
+function animate(time) {
+	// input
+	requestAnimationFrame(animate);
+
+	var delta = time - lastTime; // in ms
+	lastTime = time;
+	
+	for (var i = 0; i<hexagons.length; i++) {
+		for (var j = 0; j<hexagons[i].length; j++) {
+			hexagons[i][j].update(delta);
+		}
+	}
+
+	// render
+	renderer.render(stage);
+}
---
+++ draft 1/js/hexagon.js
@@ -0,0 +1,138 @@
+/**
+hexWidth = hexSize*0.866;
+p1 = offset+hexWidth*0.5, offset;
+p2 = offset+hexWidth, offset+hexSize*0.25;
+p3 = offset+hexWidth, offset+hexSize*0.75;
+p4 = offset+hexWidth*0.5, offset+hexSize;
+p5 = offset, offset+hexSize*0.75;
+p6 = offset, offset+hexSize*0.25;
+**/
+
+
+function hexagon () {
+
+	var hex = this;
+
+	// Settings
+	hex.textColor = 0xffffff;
+	hex.bgColor = 0x222222;
+	hex.lineColor = 0xcceeff;
+	hex.value = 1;
+	hex.size = 100;
+	hex.lineWidth = 5;
+	hex.fontSize = 26;
+	hex.x = 0;
+	hex.y = 0;
+	hex.indx = 0;
+	hex.indy = 0;
+	
+	hex.future = {};
+	hex.futureTime = null;
+
+	// Location
+	hex.row = 0;
+	hex.column = 0;
+
+	// Delegate
+	hex.delegate = null;
+
+	// Renderable
+	hex.graphics = new PIXI.Graphics();
+	hex.graphics.interactive = true;
+	hex.text = new PIXI.Text();
+	hex.text.anchor.x = 0.5;
+	hex.text.anchor.y = 0.5;
+
+	
+	// Updater
+	hex.update = function(delta) {
+
+		if (delta === undefined)
+			delta = 0;
+
+		if (hex.futureTime !== null)
+			hex._animate(delta);
+		
+		if (hex.futureTime < 0) {
+			hex.futureTime = null;
+		}
+
+		hex.graphics.clear();
+
+		var hexWidth = hex.size*0.866;
+
+		hex.graphics.lineStyle(hex.lineWidth, hex.lineColor);
+		hex.graphics.beginFill(hex.bgColor);
+
+		hex.graphics.moveTo(hex.x,              hex.y-hex.size*0.5);
+		hex.graphics.lineTo(hex.x+hexWidth*0.5, hex.y-hex.size*0.25);
+		hex.graphics.lineTo(hex.x+hexWidth*0.5, hex.y+hex.size*0.25);
+		hex.graphics.lineTo(hex.x,              hex.y+hex.size*0.5);
+		hex.graphics.lineTo(hex.x-hexWidth*0.5, hex.y+hex.size*0.25);
+		hex.graphics.lineTo(hex.x-hexWidth*0.5, hex.y-hex.size*0.25);
+		hex.graphics.lineTo(hex.x,              hex.y-hex.size*0.5);
+
+		hex.graphics.endFill();
+		
+		hex.interactive = hex.buttonMode = true;
+
+		hex.text.text = hex.value;
+		hex.text.style.fill = hex.textColor;
+		hex.text.style.fontSize = hex.fontSize;
+		hex.text.x = hex.x;
+		hex.text.y = hex.y;
+	};
+
+	hex._animate = function(delta) {
+		
+		var set = hex.futureTime-delta <= 0;
+
+		for (var key in hex.future) {
+
+			if (hex.future[key] !== null && hex.future[key] !== undefined) {
+				if (set)
+					hex[key] = hex.future[key];
+				else {
+					hex[key] += delta*(hex.future[key]-hex[key])/hex.futureTime;
+				}
+			}
+		}
+
+		hex.futureTime -= delta;
+	};
+
+	hex.graphics.click = function(ev) {
+		// notify delegate
+		console.log('clicked '+hex.row+' @ '+hex.column);
+		hex.bgColor = 0xeeefff;
+		//powers of two
+		
+		var c = 1;
+		var sum = 1;
+		var a = "";
+		for(var i = 0; i < (hex.indy + 1); i++){
+			if(i<hex.indy){
+				a += c + " + ";
+			} else {
+				a += c;
+			}
+			c = parseInt(c*((hex.indy+1)-(i+1))/(i+1));
+			sum = sum + c;
+			}
+		prompt("The sum of this row is equal to 2 raised to " + hex.indy+" i.e: " + a + " = "+sum);
+		
+		/**combinatorial**/
+		prompt("this cell is the value of "+hex.indy+" taken "+hex.indx);
+		
+	};
+
+	hex.setup = function(stage) {
+		stage.addChild(hex.graphics);
+		stage.addChild(hex.text);
+	};
+
+	hex.set = function(property, value) {
+		hex[property] = value;
+		
+	};
+}
