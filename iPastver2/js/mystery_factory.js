e73961d1d403312d1f9620bb849489c6a1259307
mtnolasco <31950502+mtnolasco@users.noreply.github.com>
2018-04-27 14:47:45 +0800

    updated list of prime numbers

--- iPastver2/js/mystery_factory.js
+++ iPastver2/js/mystery_factory.js
@@ -56,7 +56,7 @@ function mysteryFactory (delegate) {
 
 			case 'divisiblebyprime':
 			// Disable specific
-			var allow = [1,2,3,5,7,11,13,17,19];
+			var allow = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
 			for (var i = 0; i<triangle.length; i++) {
 				var disrow = true;
 				if (allow[0] == i) {
